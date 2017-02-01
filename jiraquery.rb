ebcb450cf1ce962a7c3732c6eeae89d415092297
forsaken <forsaken@4saken.org>
2017-01-31 19:11:16 -0600

    removed URL from code and put in config.

--- jiraquery.rb
+++ jiraquery.rb
@@ -21,6 +21,6 @@ client = JIRA::Client.new(options)
 #  puts "Project -> key: #{project.key}"
 #end
 
-client.Issue.jql('project=OPS and type in ("Change Request - High Risk", "Change Request - Low Risk") and created >= "2017-01-01" and created <= "2017-12-31" order by created asc').each do |issue|
+client.Issue.jql(JIRAJQL).each do |issue|
   puts "#{issue.id} - #{issue.fields['summary']} - #{issue.fields['resolution']}"
 end
