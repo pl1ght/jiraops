d17c02b5e1c9ae9e54f56f95e33fb1967b0f9cbb
pl1ght <forsaken@4saken.org>
2017-02-10 21:26:24 -0600

    def

--- jiraquery.rb
+++ jiraquery.rb
@@ -10,17 +10,13 @@ options = {
     :auth_type => :basic
 }
 
-client = JIRA::Client.new(options)
+$client = JIRA::Client.new(options)
 
-#issue = client.Issue.find("OPS-15449")
-#pp issue.issuelinks
-
-#projects = client.Project.all
-
-#projects.each do |project|
-#  puts "Project -> key: #{project.key}"
-#end
-
-client.Issue.jql('project=OPS and type in ("Change Request - High Risk", "Change Request - Low Risk") and created >= "2017-01-01" and created <= "2017-12-31" order by created asc').each do |issue|
-  puts "#{issue.id} - #{issue.fields['summary']} - #{issue.fields['resolution']}"
+def ops_change_req(jql)
+  $client.Issue.jql(jql).each do |issue|
+    reqs = "#{issue.id} - #{issue.fields['summary']} - #{issue.fields['resolution']}"
+    puts reqs
+  end
 end
+
+ops_change_req('project=OPS and type in ("Change Request - High Risk", "Change Request - Low Risk") and created >= "2017-01-01" and created <= "2017-12-31" order by created asc')
