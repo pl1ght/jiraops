7d0d8315121f9abd7c4364b048b213e4e6267520
pl1ght <forsaken@4saken.org>
2017-02-15 12:52:01 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	jiraquery.rb

--- jiraquery.rb
+++ jiraquery.rb
@@ -11,12 +11,20 @@ options = {
 }
 
 $client = JIRA::Client.new(options)
+$jql_query = 'project=OPS and type in ("Change Request - High Risk", "Change Request - Low Risk") and created >= "2017-01-01" and created <= "2017-12-31" order by created asc'
 
 def ops_change_req(jql)
   $client.Issue.jql(jql).each do |issue|
-    reqs = "#{issue.id} - #{issue.fields['summary']} - #{issue.fields['resolution']}"
-    puts reqs
+    query = []
+    query << "#{issue.id} - #{issue.fields['summary']} - #{issue.fields['resolution']}"
+    puts query
   end
 end
 
-ops_change_req('project=OPS and type in ("Change Request - High Risk", "Change Request - Low Risk") and created >= "2017-01-01" and created <= "2017-12-31" order by created asc')
+def ops_change_req_count(jql)
+  query_count = $client.Issue.jql(jql).count
+  puts query_count
+end
+
+ops_change_req($jql_query)
+ops_change_req_count($jql_query)
